9bbf470eabef45614b97ecd28e4f5a89bc5bf4ce
jeonjonghyeon <jonghyeon.jeon@ikoob.com>
2020-02-04 17:08:39 +0900

    IMP: 메시지 송수신 가능한 상태로 수정

--- app.ts
+++ app.ts
@@ -1,135 +1,48 @@
 
-import { RTMClient, LogLevel, WebClientEvent, TLSOptions, ErrorCode, UsersListArguments, WebAPICallOptions } from "@slack/client";
-import SlackEventAdapter, { verifyRequestSignature, errorCodes } from "@slack/events-api";
+import SlackEventAdapter, { verifyRequestSignature, errorCodes, createEventAdapter } from '@slack/events-api';
+import { WebClient, LogLevel, retryPolicies } from '@slack/web-api';
 import dotenv from 'dotenv';
 
+// 설정 로드 및 체크
 dotenv.config();
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
-  console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN
+  || !process.env.SLACK_TOKEN || !process.env.CLIENT_SIGNING_SECRET) {
+  console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN, SLACK_BOT_TOKEN and PORT in environment');
   process.exit(1);
 }
 
-
-let token: string | any = process.env.SLACK_BOT_TOKEN;
-
-const rtm = new RTMClient(token, {
-  // logLevel: LogLevel.ERROR,
-});
-
-rtm.on('member_joined_channel', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`Welcome to the channel, <@${event.user}>`, event)
-    console.log('Message sent successfully', reply.ts);
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error1', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well1, that was unexpected.');
-    }
-  }
-});
-
-rtm.on('connecting', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`connecting`, 'DSX5KAFFV');
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error2', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well2, that was unexpected.');
-    }
-  }
-});
-
-rtm.on('authenticated', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`authenticated`, 'DSX5KAFFV');
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error3', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well3, that was unexpected.');
-    }
-  }
-});
-
-rtm.on('connected', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`connected`, 'DSX5KAFFV');
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error4', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well4, that was unexpected.');
-    }
-  }
-});
-
-rtm.on('ready', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`ready`, 'DSX5KAFFV');
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error5', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well5, that was unexpected.');
-    }
-  }
-});
-
-rtm.on('disconnecting', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`disconnecting`, 'DSX5KAFFV');
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error6', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well6, that was unexpected.');
-    }
-  }
-});
-
-rtm.on('reconnecting', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`reconnecting`, 'DSX5KAFFV');
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error7', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well7, that was unexpected.');
-    }
-  }
-});
-
-rtm.on('disconnected', async (event) => {
-  try {
-    const reply = await rtm.sendMessage(`disconnected`, 'DSX5KAFFV');
-  } catch (error) {
-    // Check the error code, and when its a platform error, log the whole response
-    if (error.code === ErrorCode.SendMessagePlatformError) {
-      console.log('error8', error.data);
-    } else {
-      // Some other error, oh no!
-      console.log('Well8, that was unexpected.');
-    }
-  }
-});
-
+const options = {
+  logLevel: LogLevel.DEBUG, // 로그레벨
+  retryConfig: retryPolicies.fiveRetriesInFiveMinutes, //재시도(5분에 5번 시도)
+  maxRequestConcurrency: 10, // 동시요청(최대)
+};
+const appId = 'DTK7364EB';
+const web = new WebClient(process.env.SLACK_TOKEN, options);
 
 (async () => {
-  await rtm.start();
+  // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+
+  await web.chat.postMessage({ channel: appId, text: 'Hello there' });
 })();
+
+
+// Initialize using signing secret from environment variables
+const slackEvents = createEventAdapter(process.env.CLIENT_SIGNING_SECRET, { includeBody: true });
+const port: any = process.env.PORT || 3000;
+
+slackEvents.on('message', async (event) => {
+  if (event.text === "sss") {
+    await web.chat.postMessage({ channel: appId, text: 'Hello there' });
+  }
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+  
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
+
+// Start a basic HTTP server
+slackEvents.start(port).then(() => {
+  // Listening on path '/slack/events' by default
+  console.log(`server listening on port ${port}`);
+});
