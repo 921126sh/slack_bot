aadfa9f62c28f5742404fed16b0a0561f01f3379
jeonjonghyeon <jonghyeon.jeon@ikoob.com>
2020-02-11 14:32:19 +0900

    IMP: 모달 submit, action 테스트

--- app.ts
+++ app.ts
@@ -3,7 +3,6 @@ import { App, LogLevel } from '@slack/bolt';
 import dotenv from 'dotenv';
 import { loadFeatures } from './features';
 
-
 // 설정 로드 및 체크
 dotenv.config();
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN
@@ -15,16 +14,22 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT ||
 const app: App = new App({
   token: process.env.SLACK_TOKEN,
   signingSecret: process.env.CLIENT_SIGNING_SECRET,
-  logLevel: LogLevel.DEBUG
+  logLevel: LogLevel.ERROR
 });
+// 기능로드
 loadFeatures(app);
 
-
-// hi 하면 hello
-app.message('hi', async ({ message, say }) => {
-  say(`Hello, <@${message.user}>`);
+app.error((error) => {
+  console.error(JSON.stringify(error));
 });
 
+(async () => {
+  // Start the app
+  await app.start(process.env.PORT);
+  console.log('App is running! ⚡️⚡️⚡️');
+})();
+
+// ==========================================================================================
 
 app.message('me', async ({ message, context }) => {
   // Call the chat.scheduleMessage method with a token
@@ -35,156 +40,3 @@ app.message('me', async ({ message, context }) => {
     text: 'Summer has come and passed'
   });
 });
-
-app.action('button-4444', async ({ ack, say }) => {
-  ack();
-  // Update the message to reflect the action
-  console.log('button-4444');
-});
-
-// app.command("/clean", async ({ payload, ack, context, say }) => {
-//   console.log('command', payload);
-//   ack();
-
-//   const result = app.client.views.open({
-//     token: context.botToken,
-//     // Pass a valid trigger_id within 3 seconds of receiving it
-//     trigger_id: payload.trigger_id,
-//     // View payload
-//     view: {
-//       "type": "modal",
-//       "title": {
-//         "type": "plain_text",
-//         "text": "청소를 합니다",
-//         "emoji": true
-//       },
-//       "submit": {
-//         "type": "plain_text",
-//         "text": "역할배정",
-//         "emoji": true
-//       },
-//       "close": {
-//         "type": "plain_text",
-//         "text": "취소",
-//         "emoji": true
-//       },
-//       "blocks": [
-//         {
-//           "type": "section",
-//           "fields": [
-//             {
-//               "type": "mrkdwn",
-//               "text": "*청소할 조를 골라주세요~*"
-//             }
-//           ],
-//           "accessory": {
-//             "type": "radio_buttons",
-//             "initial_option": {
-//               "text": {
-//                 "type": "plain_text",
-//                 "text": "A조"
-//               },
-//               "value": "A"
-//             },
-//             "options": [
-//               {
-//                 "text": {
-//                   "type": "plain_text",
-//                   "text": "A조"
-//                 },
-//                 "value": "A"
-//               },
-//               {
-//                 "text": {
-//                   "type": "plain_text",
-//                   "text": "B조"
-//                 },
-//                 "value": "B"
-//               }
-//             ]
-//           }
-//         },
-//         {
-//           "type": "divider"
-//         },
-//         {
-//           "type": "section",
-//           "block_id": "section678",
-//           "text": {
-//             "type": "mrkdwn",
-//             "text": "청소인원"
-//           },
-//           "accessory": {
-//             "action_id": "clean_members",
-//             "type": "multi_users_select",
-//             "initial_users": [],
-//             "placeholder": {
-//               "type": "plain_text",
-//               "text": "청소할 사람"
-//             }
-//           }
-//         }
-//       ]
-//     }
-//   });
-
-//   console.log(result);
-
-// });
-
-
-
-
-
-
-app.message('hello', ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  let ddd: any = {
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ]
-  };
-
-  say(ddd);
-});
-
-app.action('button_click', ({ body, ack, say }) => {
-  // Acknowledge the action
-  ack();
-  say(`<@${body.user.id}> clicked the button`);
-});
-
-
-
-
-
-
-
-app.error((error) => {
-  // Check the details of the error to handle cases where you should retry sending a message or stop the app
-  console.error(JSON.stringify(error));
-});
-
-(async () => {
-  // Start the app
-  await app.start(process.env.PORT);
-  console.log('App is running! ⚡️⚡️⚡️');
-})();
-
-
-
-
--- features/sc_clean.ts
+++ features/sc_clean.ts
@@ -1,94 +1,268 @@
 import fs from 'fs';
-import { App } from '@slack/bolt';
+import { App, RadioButtonsAction } from '@slack/bolt';
 
 export default (app: App) => {
     let employee: any;
+    let team = [""];
+    let msg = "";
     let role = ["먼지걸레", "먼지걸레", "물걸레", "물걸레", "빗자루", "빗자루", "청소기", "화분/물티슈", "분리수거"];
     let etcRole = "물티슈";
 
-    app.command("/clean", async ({ command, ack, say }) => {
-        console.log('command', command);
-        employee = JSON.parse(fs.readFileSync(__dirname + "/employee.json", 'utf8'));
+    app.action('button_click', ({ body, ack, say }) => {
+        console.log('button_click');
+        ack();
+    });
 
-        let msg = "";
-        let cmd = command.text.toUpperCase();
-
-        if (cmd === "A") {
-            let team = employee.A;
-
-            // 역할보다 인원이 많으면 기타역할 추가
-            if (role.length < team.length) {
-                appendRole(team.length - role.length);
-            }
-
-            for (let index = 0, length = team.length; index < length; index++) {
-                // 인덱스, 직원, 역할
-                let rRoleIdx: number = 0;
-                let member: string = "";
-                let mRole: string = etcRole;
-
-                // 랜덤으로 역할 할당
-                member = team[index];
-                if (0 < role.length) {
-                    rRoleIdx = Math.floor(Math.random() * role.length);
-                    mRole = role[rRoleIdx];
-                }
-
-                // 뽑힌 역할 제거
-                role.splice(rRoleIdx, 1);
-
-                // 메시지 조립
-                msg += `${member} ===> ${mRole}\n`;
-            }
-
-            say(msg);
+    app.action('clean-modal_option_click', ({ action, ack }) => {
+        console.log('clean-modal_option_click action ::: ', action);
+        if (action.type === "radio_buttons") {
+            action.selected_option.value;
+            console.log('action::: ', action);
+            console.log('action.selected_option.value ::: ', action.selected_option.value);
         }
-        else if (cmd === "B") {
-            let team = employee.B;
-
-            // 역할보다 인원이 많으면 기타역할 추가
-            if (role.length < team.length) {
-                appendRole(team.length - role.length);
-            }
-
-            for (let index = 0, length = team.length; index < length; index++) {
-                // 인덱스, 직원, 역할
-                let rRoleIdx: number = 0;
-                let member: string = "";
-                let mRole: string = etcRole;
-
-                // 랜덤으로 역할 할당
-                member = team[index];
-                if (0 < role.length) {
-                    rRoleIdx = Math.floor(Math.random() * role.length);
-                    mRole = role[rRoleIdx];
-                }
-
-                // 뽑힌 역할 제거
-                role.splice(rRoleIdx, 1);
-
-                // 메시지 조립
-                msg += `${member} ===> ${mRole}\n`;
-            }
-
-            say(msg);
-        }
-        // help displays this message
-        else if (cmd === "HELP") {
-            say("\"/clean A\" 를 입력하면 A조 청소역할분담이 나옵니다. :robot_face:");
-        }
-        else {
-            say("\"/clean help\" 로 명령을 확인해주세요! :robot_face:");
+        else if (action.type === "button") {
+            console.log('action::: ', action);
+            console.log('action.value ::: ', action.value);
         }
 
         ack();
-        console.log("Entered into the app.command for /clean");
+    });
+
+    // 모달에 대한 콜백아이디로 submit 요청을 처리한다.
+    app.view('clean-modal', async ({ ack, body, view, context }) => {
+        ack();
+        console.log(JSON.stringify(view.state));
+
+        const user = body.user.id;
+
+        app.client.chat.postMessage({
+            token: context.botToken,
+            channel: user,
+            text: msg
+        });
+    });
+
+    app.command("/ikoob", async ({ payload, ack, context, say }) => {
+        ack();
+        let fnNm = payload.text.split(' ')[0];
+        if (fnNm === "청소") {
+            // let cmd = payload.text.split(' ')[1].toUpperCase();
+            // employee = JSON.parse(fs.readFileSync(__dirname + "/employee.json", 'utf8'));
+
+            // if (cmd === "A") {
+            //     team = employee.A;
+            //     msg = randomClean();
+            // }
+            // else if (cmd === "B") {
+            //     team = employee.B;
+            //     msg = randomClean();
+            // }
+            // else if (cmd === "HELP") {
+            //     msg = "\"/청소 A\" 를 입력하면 A조 청소역할분담이 나옵니다. :robot_face:";
+            // }
+            // else {
+            //     msg = "\"/청소 help\" 로 명령을 확인해주세요! :robot_face:";
+            // }
+
+
+
+            const result = await app.client.views.open({
+                token: context.botToken,
+                trigger_id: payload.trigger_id,
+                view: {
+                    "type": "modal",
+                    "callback_id": "clean-modal",
+                    "title": {
+                        "type": "plain_text",
+                        "text": "청소를 합시다",
+                        "emoji": true
+                    },
+                    "close": {
+                        "type": "plain_text",
+                        "text": "취소",
+                        "emoji": true
+                    },
+                    "blocks": [
+                        {
+                            "type": "section",
+                            "text": {
+                                "type": "mrkdwn",
+                                "text": "This is a section block with radio button accessory"
+                            },
+                            "accessory": {
+                                "action_id": "clean-modal_option_click",
+                                "type": "radio_buttons",
+                                "options": [
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "A조"
+                                        },
+                                        "value": "A",
+                                    },
+                                    {
+                                        "text": {
+                                            "type": "plain_text",
+                                            "text": "B조"
+                                        },
+                                        "value": "B"
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            "type": "actions",
+                            "elements": [
+                                {
+                                    "action_id": "clean-modal_option_click",
+                                    "type": "button",
+                                    "text": {
+                                        "type": "plain_text",
+                                        "text": "Button",
+                                        "emoji": true
+                                    },
+                                    "value": "click_me_123"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            });
+
+            // const result = await app.client.views.open({
+            //     token: context.botToken,
+            //     trigger_id: payload.trigger_id,
+            //     view: {
+            //         "type": "modal",
+            //         "callback_id": "clean-modal",
+            //         "title": {
+            //             "type": "plain_text",
+            //             "text": "청소를 합니다",
+            //             "emoji": true
+            //         },
+            //         "submit": {
+            //             "type": "plain_text",
+            //             "text": "역할배정",
+            //             "emoji": true
+            //         },
+            //         "close": {
+            //             "type": "plain_text",
+            //             "text": "취소",
+            //             "emoji": true
+            //         },
+            //         "blocks": [
+            //             {
+            //                 "type": "divider"
+            //             },
+            //             {
+            //                 "type": "input",
+            //                 "label": {
+            //                     "type": "plain_text",
+            //                     "text": "User(s)"
+            //                 },
+            //                 "element": {
+            //                     "type": "multi_users_select",
+            //                     "placeholder": {
+            //                         "type": "plain_text",
+            //                         "text": "Where should the poll be sent?"
+            //                     }
+            //                 }
+            //             },
+            //             {
+            //                 "block_id": "section-clean",
+            //                 "type": "actions",
+            //                 "elements": [
+            //                     {
+            //                         "action_id": "button_click",
+            //                         "type": "button",
+            //                         "text": {
+            //                             "type": "plain_text",
+            //                             "text": "Button",
+            //                             "emoji": true
+            //                         },
+            //                         "value": "click_me_123"
+            //                     },
+            //                     {
+            //                         "action_id": "clean-modal_option_click",
+            //                         "type": "radio_buttons",
+            //                         "options": [
+            //                             {
+            //                                 "text": {
+            //                                     "type": "plain_text",
+            //                                     "text": "Option 1"
+            //                                 },
+            //                                 "value": "option 1",
+            //                                 "description": {
+            //                                     "type": "plain_text",
+            //                                     "text": "Description for option 1"
+            //                                 }
+            //                             },
+            //                             {
+            //                                 "text": {
+            //                                     "type": "plain_text",
+            //                                     "text": "Option 2"
+            //                                 },
+            //                                 "value": "option 2",
+            //                                 "description": {
+            //                                     "type": "plain_text",
+            //                                     "text": "Description for option 2"
+            //                                 }
+            //                             },
+            //                             {
+            //                                 "text": {
+            //                                     "type": "plain_text",
+            //                                     "text": "Option 3"
+            //                                 },
+            //                                 "value": "option 3",
+            //                                 "description": {
+            //                                     "type": "plain_text",
+            //                                     "text": "Description for option 3"
+            //                                 }
+            //                             }
+            //                         ]
+            //                     }
+            //                 ]
+            //             }
+            //         ]
+            //     }
+            // });
+        }
 
     });
 
-    function appendRole(len: number): void {
-        for (let index = 0; index < len; index++) {
-            role.push(etcRole);
+    /**
+     * 청소 역할 랜덤으로 만들기
+     */
+    function randomClean(): string {
+        let msg = "";
+        // 역할보다 인원이 많으면 기타역할 추가
+        if (role.length < team.length) {
+            let len = role.length - team.length;
+            for (let index = 0; index < len; index++) {
+                role.push(etcRole);
+            }
         }
+
+        for (let index = 0, length = team.length; index < length; index++) {
+            // 인덱스, 직원, 역할
+            let rRoleIdx: number = 0;
+            let member: string = "";
+            let mRole: string = etcRole;
+
+            // 랜덤으로 역할 할당
+            member = team[index];
+            if (0 < role.length) {
+                rRoleIdx = Math.floor(Math.random() * role.length);
+                mRole = role[rRoleIdx];
+            }
+
+            // 뽑힌 역할 제거
+            role.splice(rRoleIdx, 1);
+
+            // 메시지 조립
+            msg += `${member} ===> ${mRole}\n`;
+        }
+
+        return msg;
     }
 }
